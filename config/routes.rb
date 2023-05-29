087c787076ec02e094aec432dcaac7eb9965e605
nttVu <thevu@advn.vn>
2023-05-29 17:01:37 +0700

    update api post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
   namespace :api do 
     namespace :v1 do
       resources :books
+      resources :posts
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
